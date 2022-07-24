d3714dd8c90161a4363a47ac4e863b3cb24f0133
nikothegamer <95710125+nikothegamer100@users.noreply.github.com>
2022-07-24 11:19:33 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,5 @@
+link = "https://github.com/knightlab-analyses/qurro-mackerel-analysis/blob/master/AnalysisOutput/qurro-plot.qzv"
 print("test")
 print("another test")
+
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
